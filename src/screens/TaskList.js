d0b24b6ec9cead7ebeb183007659c4981313cead
SuzaneChimenass <suzanecamila879@gmail.com>
2025-03-17 20:30:39 -0300

    Adicionando componente Task e FlatList para listagem de itens

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,23 +1,52 @@
-import { View, Text, ImageBackground, StyleSheet, TouchableOpacity } from "react-native";
-import moment from "moment-timezone"
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity, FlatList} from "react-native"
+
+import moment from 'moment-timezone'
 import 'moment/locale/pt-br'
 
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import todayImage from '../../assets/imgs/today.jpg'
+import Task from "../components/Task"
 
-export default function TaskList(){
-    
+export default function TaskList() {
+
     const today = moment().tz("America/Sao_Paulo")
         .locale("pt-br").format('ddd, D [de] MMMM')
+
+    const tasks=[
+        {
+            id:Math.random(),
+            desc: 'Elaborar o Mer do TCC',
+            estimateAt:new Date(),
+            doneAt: new Date()
+        },
+        {
+            id:Math.random(),
+            desc: 'Ajustar o FIGMA',
+            estimateAt:new Date(),
+            doneAt: null
+        },
+        {
+            id:Math.random(),
+            desc: 'Revisar a documentação do projeto',
+            estimateAt:new Date(),
+            doneAt: new Date()
+        },
+        {
+            id:Math.random(),
+            desc: 'Organizar o Trello',
+            estimateAt:new Date(),
+            doneAt: null
+        },
+    ]
 
     return(
         <View style={styles.container}>
             <ImageBackground source={todayImage} style={styles.background}>
-                
-                <View style={styles.IconBar}>
-                <TouchableOpacity onPress={() => console.log('oi')}>
-                        <Icon name="eye" size={20} color={'#fff'}></Icon>
+
+                <View style={styles.iconBar}>
+                    <TouchableOpacity onPress={() => console.log('oi')}>
+                        <Icon name="eye" size={20} color={'#fff'} />
                     </TouchableOpacity>
                 </View>
 
@@ -27,62 +56,66 @@ export default function TaskList(){
                 </View>
 
             </ImageBackground>
-
             <View style={styles.taskList}>
-                <Text>Task #01</Text>
+                <FlatList
+                    data={tasks}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={({item}) => <Task{...item} />}
+                />
             </View>
+
             <TouchableOpacity style={styles.addButton}
                 activeOpacity={0.7}
-                onPress={()=> console.warn("+")}>
+                onPress={() => console.warn("+")}>
                 
-                <Icon name="plus" size={20} color={"#fff"}/>
+                <Icon name="plus" size={20} color={"#fff"} />
 
             </TouchableOpacity>
+
         </View>
     )
 }
-
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1
     },
     background: {
-        flex: 3,
+        flex: 3
     },
     taskList: {
-        flex: 7,
-    },
+        flex: 7
+    }, 
     titleBar: {
-        flex:1,
-        justifyContent:'flex-end'
+        flex: 1,
+        justifyContent: 'flex-end'
     },
-    title:{
-        color:'white',
-        fontSize:50,
-        marginLeft:20,
-        marginBottom:20
+    title: {
+        color: 'white',
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20
     },
     subtitle: {
-        color:'white',
-        fontSize:20,
-        marginLeft:20,
-        marginBottom:30
+        color: 'white',
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
     },
-    addButton:{
-        position:'absolute',
+    addButton: {
+        position: 'absolute',
         right: 30,
-        bottom:50,
-        width:50,
-        height:50,
-        borderRadius:50,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
         backgroundColor: '#B13B44',
-        justifyContent:'center',
-        alignItems:'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    IconBar: {
-        flexDirection:'row',
-        marginHorizontal:20,
-        justifyContent:'flex-end',
-        marginTop:20
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: 20
     }
 })
